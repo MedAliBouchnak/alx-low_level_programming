d08f75aeca1146a76607e55bb415bee03a4313a3
Mohamed Ali <mohamedalibouchnak.epi@gmail.com>
2023-09-12 15:41:04 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+
+	for (char C = 'a'; C <= 'z'; C++)
+		_putchar(C);
+
+	_putchar('\n');
+}
