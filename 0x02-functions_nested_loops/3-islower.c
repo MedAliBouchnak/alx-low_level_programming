3d72506b84970bd49a0114e383cc8644e9516299
Mohamed Ali <mohamedalibouchnak.epi@gmail.com>
2023-09-12 16:38:39 -0700

    my first commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - check if char is lowercase
+ * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
 
-int _islower(int C)
+int _islower(int c)
 {
-	if (C >= 'a' && C <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
 }
-
