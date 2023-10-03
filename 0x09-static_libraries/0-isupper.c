f4ee4a6c4f90465376d5865e132804398b3edddd
Mohamed Ali <mohamedalibouchnak.epi@gmail.com>
2023-10-02 17:04:05 -0700

    updating

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* _isupper - check the code.
+* @c: Ras zebi fi sormek
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
