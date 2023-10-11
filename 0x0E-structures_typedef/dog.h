6f148a55404aeef5062ba71c8ddf389e9339ca63
Mohamed Ali <mohamedalibouchnak.epi@gmail.com>
2023-10-10 18:18:11 -0700

    last push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Longer description for dog lovers
+ * Description: Longer description
  */
 struct dog
 {
@@ -15,5 +15,17 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
